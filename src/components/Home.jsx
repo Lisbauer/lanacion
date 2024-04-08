7520c88e96c66d591026f79a3df3d7ca8a9073ab
Lisbauer <lisa.bauer@hotmail.com>
2024-04-08 20:04:14 -0300

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <>
+
+        <div className='home_title'>
+            <h2>Volanta. Esto es un titulo de la novedad con unas cuantas lineas que ocupar</h2>
+        </div>
+
+        <div className='anexo'>
+            <h4>Anexo 1</h4>
+        </div>
+
+    </>
+  )
+}
+
+export default Home
