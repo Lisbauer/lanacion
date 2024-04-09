486b91bcd8eb49a3426ab7e221ceb702051f918f
Lisbauer <lisa.bauer@hotmail.com>
2024-04-09 18:26:10 -0300

    SEO incorporation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,39 @@
-import React from 'react'
-import './home.css'
+import React from "react";
+import "./home.css";
 
 const Home = () => {
   return (
     <>
+      <div className="homeTitle">
+        <h2>Cuándo se podrá ver un eclipse solar total en Florida</h2>
+      </div>
 
-        <div className='homeTitle'>
-            <h2>Cuándo se podrá ver un eclipse solar total en Florida</h2>
-            
-        </div>
+      <div className="eclipseNew">
+        <p>
+          El fenómeno astronómico del 8 de abril mantuvo entusiasmados a los
+          estadounidenses; más de 30 millones de personas lo presenciaron en
+          vivo; el próximo evento tendrá lugar en agosto de 2045
+        </p>
+        <p style={{ fontSize: "0.9em", color: "grey" }}>
+          8 de abril de 2024 17:56
+        </p>
+        <img
+          className="eclipseImage"
+          src="../images/eclipse.png"
+          alt="eclipse"
+        />
+        <p>
+          Millones de personas en todo Estados Unidos vivieron en primera
+          persona el eclipse solar total del 8 de abril de 2024
+        </p>
+        <p className="eclipseAuthor">Foto de Rick Meyers en Unsplash</p>
+      </div>
 
-        <div className='eclipseNew'>
-            <p>El fenómeno astronómico del 8 de abril mantuvo entusiasmados a los estadounidenses; más de 30 millones de personas lo presenciaron en vivo; el próximo evento tendrá lugar en agosto de 2045</p>
-            <p style={{fontSize:'0.9em', color:'grey'}}>8 de abril de 2024 17:56</p>
-            <img className='eclipseImage' src="../images/eclipse.png" alt="eclipse" />
-            <p>Millones de personas en todo Estados Unidos vivieron en primera persona el eclipse solar total del 8 de abril de 2024</p>
-            <p className='eclipseAuthor'>Foto de Rick Meyers en Unsplash</p>
-        </div>
-        
-        <div className='publicity'>
-            <h4>PUBLICIDAD</h4>
-        </div>
-
+      <div className="publicity">
+        <h4>PUBLICIDAD</h4>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './scss/index.scss';
+import './components/fonts.css';
 import Navbar from './components/Navbar.jsx';
 import Home from './components/Home.jsx';
 import ArticleCard from './components/ArticleCard.jsx';
 import Footer from './components/Footer.jsx';
-import './components/fonts.css';
+
 import CardGroupColor from './components/CardGroupColor.jsx';
 import FirstNew from './components/FirstNew.jsx';
 import LeftFocusedArticle from './components/LeftFocusedArticle.jsx';
